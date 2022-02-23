4372c1aab5a1a48a7d52c324abf157d0180e8af9
Todd W <toddsurfs@icloud.com>
2022-02-22 21:03:14 -0800

    send messages hooked up to twilio

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const twilio = require("twilio");
 
-app.get("/", (req, res) => {
-  res.end("Hi");
+const token = process.env.TWILIO_AUTH_TOKEN;
+const sid = process.env.TWILIO_ACCOUNT_SID;
+const from = process.env.TWILIO_FROM_NUMBER;
+
+app.use(bodyParser.json());
+
+const client = twilio(sid, token, {
+  lazyLoading: true
+});
+
+app.post("/sms", (req, res) => {
+  (async () => {
+    const message = await client.messages.create({
+      body: req.body.message,
+      from,
+      to: req.body.to
+    });
+
+    res.end(JSON.stringify(message));
+  })();
 });
 
 app.listen(process.env.PORT || 3000);
